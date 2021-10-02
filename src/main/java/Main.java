2302008235c53460ac44487073b2ffff307b0897
asya afanasyeva <afanasyeva656@gmail.com>
2021-10-02 22:39:56 +0200

    Updated Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        Counter counter = new Counter();
+        Timer timer = new Timer(counter);
+        Waiter fiveSecWaiter = new Waiter(counter);
 
+        new Thread(timer).start();
+        new Thread(fiveSecWaiter).start();
+
+        System.out.println("Поехали!");
     }
 }
